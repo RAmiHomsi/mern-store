022e525007c6fe309222a361929978809b33d4de
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-03-09 23:01:42 +0100

    deploy4

--- api/index.js
+++ api/index.js
@@ -24,6 +24,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://keen-malabi-f3e7cb.netlify.app",
+      "https://mern-store-front-v2ax.onrender.com",
       "https://mern-store-9oc8.onrender.com",
       "https://mern-store-9oc8.onrender.com/api",
     ],
