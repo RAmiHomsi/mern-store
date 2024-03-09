2307279cbf5437e04226398408cdf4f0e1b47863
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-03-10 00:09:14 +0100

    deploy6

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,18 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": {
-        target: "https://mern-store-9oc8.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-      "/uploads/": {
-        target: "https://mern-store-9oc8.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
+      "/api/": "https://mern-store-9oc8.onrender.com",
+      "/uploads/": "https://mern-store-9oc8.onrender.com",
     },
   },
 });
